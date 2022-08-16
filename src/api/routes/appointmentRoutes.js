6af6149ab7f4df4ece33ad4238c0a179b24c9c43
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-08-16 14:57:01 +0530

    implement appointment routes

--- src/api/routes/appointmentRoutes.js
+++ src/api/routes/appointmentRoutes.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
-const verifyToken = require("../verifyToken/verifyToken");
 const {
     addAppointment,
     
-} = require("../controllers/appointment.controller");
-const Appointment = require("../model/appointmentModel");
+} = require("../controllers/appointmentController");
 
 //define user routes
-router.post("/add", verifyToken, addAppointment);
+router.post("/add", addAppointment);
 module.exports = router;
