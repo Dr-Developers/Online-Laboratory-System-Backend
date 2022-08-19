b9546c1aad90d5522ca5d47b6e84d8b56cce99f3
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-08-19 14:39:48 +0530

    implement appointment model

--- src/api/models/appointmentModel.js
+++ src/api/models/appointmentModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const appointmentSchema = new mongoose.Schema({
         date: { 
-            type: Date,
+            type: String,
             required: true },
         time: { 
             type: String, 
