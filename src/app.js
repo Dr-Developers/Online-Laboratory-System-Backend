dcc1ac6c8d723cb94458781a866ac51650b7b271
Udul-D <uduldulsara@gmail.com>
2022-08-20 15:29:12 +0530

    Implement Appjs InvtRoutes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const patientRoute = require("./api/routes/patientRoutes");
 const appointmentRoute = require("./api/routes/appointmentRoutes");
 const reportRoute = require("./api/routes/reportRoutes");
+const inventoryRoute = require("./api/routes/inventoryRoutes");
 
 const app = express();
 app.use(express.json());
@@ -26,3 +27,4 @@ app.listen(process.env.PORT || 5000, () => {
 app.use("/api/patient", patientRoute);
 app.use("/api/appointment", appointmentRoute);
 app.use("/api/report", reportRoute);
+app.use("/api/inventory", inventoryRoute);
