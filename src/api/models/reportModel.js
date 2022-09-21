f2a93cf04326171c6889d1ce90d0db5d7390666c
Heshani Sandanika <74381597+heshani9918@users.noreply.github.com>
2022-09-21 13:32:44 +0530

    Implement report model

--- src/api/models/reportModel.js
+++ src/api/models/reportModel.js
@@ -3,15 +3,13 @@ const mongoose = require("mongoose");
 const reportSchema = new mongoose.Schema({
         firstName: { type: String, required: true },
         lastName: { type: String, required: true },
-        gender: { type: String, required: true, unique: true },
-        date: { type: String, required: true, },
-        age: { type: Number, required: true, unique: true },
+        date: { type: String, required: true },
+        age: { type: Number, required: true},
         nic: { type: String, required: true, unique: true },
+        Gender: {type: String, required: true},
         phoneNumber: { type: String, required: true },
-        testName: { type: String, required: true, unique: true },
-        test: { type: String, required: true, unique: true },
-        result: { type: String, required: true, unique: true },
-        normalValues: { type: String, required: true, unique: true },
+        testName: { type: String, required: true},
+        testData: [[{test:String, result:String, normalValues:String}]]
     },
 
     { timestamps: true }
