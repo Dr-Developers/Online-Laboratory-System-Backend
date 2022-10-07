ff82b997cf2809a715d1f3708fad8845bb483370
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-10-07 11:56:56 +0530

    appointment routes

--- src/api/routes/appointmentRoutes.js
+++ src/api/routes/appointmentRoutes.js
@@ -1,24 +1,23 @@
 const router = require("express").Router();
 const {
-    addAppointment,
-    getAppointments,
-    getOneAppointment,
-    getDateAndTime,
+	addAppointment,
+	getAppointments,
+	getOneAppointment,
+	getDateAndTime,
 	updateAppointment,
-    deleteAppointment,
+	deleteAppointment,
 } = require("../controllers/appointmentController");
 
 const {
-    verifyTokenAuthorization,
-    verifyTokenAndLabAss,
+	verifyTokenAuthorization,
+	verifyTokenAndLabAss,
 } = require("../verifyToken/verifyToken");
-
 
 //define user routes
 router.post("/add", verifyTokenAuthorization, addAppointment);
 router.get("/", verifyTokenAndLabAss, getAppointments);
 router.get("/:id", verifyTokenAuthorization, getOneAppointment);
-router.get("/check", verifyTokenAuthorization, getDateAndTime);
+router.post("/check", verifyTokenAuthorization, getDateAndTime);
 router.put("/update/:id", verifyTokenAuthorization, updateAppointment);
 router.delete("/delete/:id", verifyTokenAuthorization, deleteAppointment);
 
