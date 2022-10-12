1e6ef9d24dc9530c7ad64171b6c211eeb5d8c193
Heshani Sandanika <74381597+heshani9918@users.noreply.github.com>
2022-10-13 01:50:25 +0530

    Implement report controller

--- src/api/controllers/reportController.js
+++ src/api/controllers/reportController.js
@@ -2,68 +2,85 @@ const Report = require("../models/reportModel");
 const { reportValidation } = require("../validations/reportValidation");
 
 //add report function
-const addReport = async(req, res) => {
-    //validate the report input fields
-console.log("dd");
-    // const { error } = reportValidation(req.body.data);
-    // if(error) {
-    //     return res.send({ message: error["details"][0]["message"] });
-    // }
-console.log("ee");
-   // checking the data in the console
-    console.log(req.body);
+const addReport = async (req, res) => {
+	//validate the report input fields
+	console.log("dd");
+	// const { error } = reportValidation(req.body.data);
+	// if(error) {
+	//     return res.send({ message: error["details"][0]["message"] });
+	// }
+	console.log("ee");
+	// checking the data in the console
+	console.log(req.body);
 
-    //to check report already exist
-    const reportExist = await Report.findOne({
-        nic: req.body.data.nic,
-        date: req.body.data.date,
-        testName: req.body.data.testName,
-    });
+	//to check report already exist
+	const reportExist = await Report.findOne({
+		nic: req.body.data.nic,
+		date: req.body.data.date,
+		testName: req.body.data.testName,
+	});
 
-    if(reportExist) {
-        // console.log(nicExist);
-        return res.status(400).send({ message: "Report already exist" });
-    }
+	if (reportExist) {
+		// console.log(nicExist);
+		return res.status(400).send({ message: "Report already exist" });
+	}
 
 	try {
 		console.log("ok");
-    //assign data to the model
-    const report = await new Report({
-        firstName: req.body.data.firstName,
-        lastName: req.body.data.lastName,
-        Gender: req.body.data.Gender,
-        date: req.body.data.date,
-        age: req.body.data.age,
-        nic: req.body.data.nic,
-        phoneNumber: req.body.data.phoneNumber,
-        testName: req.body.data.testName,
-        testData: req.body.data.testData,
-    }).save()
+		//assign data to the model
+		const report = await new Report({
+			firstName: req.body.data.firstName,
+			lastName: req.body.data.lastName,
+			Gender: req.body.data.Gender,
+			date: req.body.data.date,
+			age: req.body.data.age,
+			nic: req.body.data.nic,
+			phoneNumber: req.body.data.phoneNumber,
+			testName: req.body.data.testName,
+			testData: req.body.data.testData,
+		}).save();
 
-    console.log("ss",report);
-        //save the data in the database
-        //console.log("Report Saved Successfully");
-        //const savedReport = await report.save();
-        return res.status(200).json(report);
-        console.log("Report Saved Successfully",savedReport);
-    } catch(error) {
-        //error handling
-        return res.status(400).send({ message: error });
-    }
-};
-
-const getreports = async (req, res) => {
-	try {
-		// taking all the report data
-		const allReports = await Report.find();
-		res.send(allReports); // sending the taken data
-	} catch (err) {
-		res.status(400).json(err.message); // error handling
+		console.log("ss", report);
+		//save the data in the database
+		//console.log("Report Saved Successfully");
+		//const savedReport = await report.save();
+		return res.status(200).json(report);
+		console.log("Report Saved Successfully", savedReport);
+	} catch (error) {
+		//error handling
+		return res.status(400).send({ message: error });
 	}
 };
 
-const getOneReport = async (req, res) => {
+// const getreports = async (req, res) => {
+// 	try {
+// 		// taking all the report data
+// 		const allReports = await Report.find();
+// 		res.send(allReports); // sending the taken data
+// 	} catch (err) {
+// 		res.status(400).json(err.message); // error handling
+// 	}
+// };
+
+const getReportsByFilter = async (request, response) => {
 	try {
+		let { searchFilter } = request.body;
+		let reportViewModel = [];
+		if (searchFilter) {
+			reportViewModel = await Report.find({
+				firstName: { $regex: searchFilter, $options: "i" },
+			});
+		} else {
+			reportViewModel = await Report.find();
+		}
+		response.json(reportViewModel);
+	} catch (error) {}
+}; //1
+
+const getOneReport = async (req, res) => {
+
+	try {
+		
 		// taking the relavent report
 		const takenReport = await Report.findOne({ _id: req.params.id });
 
@@ -88,9 +105,7 @@ const deleteReport = async (req, res) => {
 		if (!report) {
 			res.status(404).json("Report not found !"); // if not found report
 		} else {
-			const deletedReport = await Report.findByIdAndDelete(
-				reportID,
-			); // if found delete report
+			const deletedReport = await Report.findByIdAndDelete(reportID); // if found delete report
 			res.status(200).json(deletedReport);
 			console.log("Report Deleted Successfully !");
 		}
@@ -100,7 +115,6 @@ const deleteReport = async (req, res) => {
 };
 
 const updateReport = async (req, res) => {
-	
 	try {
 		// setting the updated details of the specific user and update database
 		const updatedReport = await Report.findByIdAndUpdate(
@@ -126,9 +140,10 @@ const updateReport = async (req, res) => {
 };
 
 module.exports = {
-    addReport,
-    getreports,
+	addReport,
+	//getreports,
 	getOneReport,
 	deleteReport,
 	updateReport,
+	getReportsByFilter,
 }; //export functions
