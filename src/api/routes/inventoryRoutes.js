c9e9fbc823dc7d43dc4fdc5e3b8dfb01dbb38424
Udul-D <uduldulsara@gmail.com>
2022-10-13 00:14:42 +0530

    Update Inventory routes

--- src/api/routes/inventoryRoutes.js
+++ src/api/routes/inventoryRoutes.js
@@ -10,7 +10,7 @@ const {
 const { verifyTokenAndLabAss } = require("../verifyToken/verifyToken");
 
 // defining routes
-router.post("/add", verifyTokenAndLabAss, addInventory);
+router.post("/add", addInventory);
 router.get("/", verifyTokenAndLabAss, getInventory);
 router.get("/:id", verifyTokenAndLabAss, getOneInventory);
 router.put("/update/:id", verifyTokenAndLabAss, updateInventory);
